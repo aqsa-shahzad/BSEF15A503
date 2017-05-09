45f53097770b3b90e00e7cd006b74d4a95cfcf8c
aqsa-shahzad <aqsashahzadffc@gmail.com>
2017-05-09 06:09:57 +0500

    Add files via upload

---
+++ priority.py (new).py
@@ -0,0 +1,33 @@
+#!usr/bin/env python
+
+
+priority of processes=[]
+bur_time=[]
+arr_time=[]
+def mypriorityfunc(p_processes.index)
+   newprocesses=list[]
+   p_element=p_processes[index]
+   le=len(p_processes)
+   count=0; 
+for z in range (0,le):
+	pr_no=input("Priority number:")
+	priority number.append(pr_no)
+	arr_time=input("Arrival Time:")
+	arrival_time.append(arr_time]
+	bur_time=input("Burst Time:")
+	burst_time.append(bur_time)
+	processes[priority number[z]] = [z+1 , arr_time[z] , bur_time[z]]
+
+print ("Priority number        Arrival Time           Burst Time")
+for z in range (0,n):
+	print (priority number[z], "\t\t\t" , arr_time[z] ,  "\t\t\t" , bur_time[z]) ) 
+
+bur_time.sorted_t()
+total_t = processes.get(priority number_time[0])[1]
+for j in range(le)
+n = input("Enter priority of  processes:")
+    o=processes[j]
+	if o < p_elem or (o==p_elem && z<=index)
+	priority of processes.append(n)
+	count+=le
+  
